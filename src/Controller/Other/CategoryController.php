5da97818a9e1c197e40280c9f335ba2068af1135
Wixkun <alexismontoya2004@gmail.com>
2024-11-26 11:01:21 +0100

    newController

--- src/Controller/Other/CategoryController.php
+++ src/Controller/Other/CategoryController.php
@@ -6,12 +6,33 @@ namespace App\Controller\Other;
 
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\Routing\Attribute\Route;
+use SYmfony\Component\HttpFoundation\Response;
+use App\Repository\CategoryRepository;
 
 class CategoryController extends AbstractController
 {
-    #[Route(path: '/categories', name: 'page_categories')]
-    public function categories()
+    #[Route(path: '/categories/{id}', name: 'page_categories')]
+    public function categories(
+        string $id,
+        CategoryRepository $categoryRepository,
+    ): Response
     {
-        return $this->render('other/discover.html.twig');
+        $category = $categoryRepository->find($id);
+
+        return $this->render('other/category.html.twig', [
+            'category' => $category,
+        ]);
+    }
+
+    #[Route(path: '/discover', name: 'page_discover')]
+    public function discover(
+        CategoryRepository $categoryRepository,
+    ): Response
+    {
+        $categories = $categoryRepository->findAll();
+
+        return $this->render('other/discover.html.twig', [
+            'categories' => $categories,
+        ]);
     }
 }
